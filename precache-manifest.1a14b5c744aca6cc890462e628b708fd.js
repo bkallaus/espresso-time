fe3e2d12008ad1e1285ea2a907e60afff6ecb822
bkallaus <benkallaus@gmail.com>
2019-06-23 19:53:23 -0500

    Updates

---
+++ precache-manifest.1a14b5c744aca6cc890462e628b708fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2d159f13f448e98120a0438e75cbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/static/js/main.b9e2d134.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
