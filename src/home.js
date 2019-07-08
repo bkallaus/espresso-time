1dc7422e5e569bd5c3833fa2c80abc77c4f927a1
bkallaus <benkallaus@gmail.com>
2019-07-07 21:19:28 -0500

    Start of styling the homepage.

--- src/home.js
+++ src/home.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  
+  margin: 16px;
+  height: 100vh;
+  width: 100%;
+`;
+
+const StyledCard  = styled.div`
+  padding: 12px;
+  height: 100px;
+  width: 80px;
+`;
 
 function Home() {
   return (
-    <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/pour-over">Pour Over</Link></li>
-        <li><Link to="/espresso">Espresso</Link></li>
-    </ul>
+    <StyledContainer>
+        <StyledCard><Link to="/pour-over">Pour Over</Link></StyledCard>
+        <StyledCard><Link to="/espresso">Espresso</Link></StyledCard>
+    </StyledContainer>
   );
 }
 
