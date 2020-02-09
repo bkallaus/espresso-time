80ec02a97ca2b7c7e52dbf0ba63d8fec05beaceb
bkallaus <benkallaus@gmail.com>
2020-02-09 16:47:27 -0600

    Updates

---
+++ precache-manifest.248ce71629b3ae5d2a5fe03a8719c684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e567d47bcc2ec029a06fdaaf07d01e9",
+    "url": "/espresso-time/index.html"
+  },
+  {
+    "revision": "e0ef17d3c1d310a82448",
+    "url": "/espresso-time/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fee1d1ee9ba94db9f3aa",
+    "url": "/espresso-time/static/js/2.f7f78ea5.chunk.js"
+  },
+  {
+    "revision": "e0ef17d3c1d310a82448",
+    "url": "/espresso-time/static/js/main.b5d75fac.chunk.js"
+  },
+  {
+    "revision": "94d345eaf78bcd25f21f",
+    "url": "/espresso-time/static/js/runtime~main.e59f9cda.js"
+  },
+  {
+    "revision": "9aa9ef25b7cc2f98215c4e5d977804bf",
+    "url": "/espresso-time/static/media/coffee-blur.9aa9ef25.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/espresso-time/precache-manifest.ee5d22e5a284b345435ec1dcfefe6a3f.js"
+  "/espresso-time/precache-manifest.248ce71629b3ae5d2a5fe03a8719c684.js"
 );
 
 self.addEventListener('message', (event) => {
