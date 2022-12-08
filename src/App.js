d056d7e1d9281059379a619ec09c871bb980c558
bkallaus <benkallaus@gmail.com>
2022-12-07 21:03:01 -0600

    feat: start of rework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
-import AppRouter from './app-router';
+import styled from 'styled-components';
 
-const App = () => <AppRouter />;
+import { ChakraProvider } from '@chakra-ui/react';
+import TimerButton from './component/timer-button';
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  flex-wrap: wrap;
+  height: 100%;
+`;
+
+const StyledItem = styled.div`
+    margin: 40px 16px;
+`;
+
+const App = () => (
+  <ChakraProvider>
+    <div>
+      <StyledItem>
+        16g of grind
+      </StyledItem>
+      <StyledItem>
+        25 seconds of pull time
+      </StyledItem>
+      <StyledItem>
+        24g of espresso
+      </StyledItem>
+      <TimerButton />
+    </div>
+  </ChakraProvider>
+);
+
 
 export default App;
